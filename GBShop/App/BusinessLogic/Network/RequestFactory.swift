1d67b858965170b119205c100a5a993c929bdfcc
Maxim Losev <losev-maksim@mail.ru>
2022-08-07 12:56:35 +0300

    Add functional from manual

---
+++ GBShop/App/BusinessLogic/Network/RequestFactory.swift
@@ -0,0 +1,30 @@
+//
+//  RequestFactory.swift
+//  GBShop
+//
+//  Created by Максим Лосев on 05.08.2022.
+//
+
+import Alamofire
+
+class RequestFactory {
+    
+    func makeErrorParser() -> AbstractErrorParser {
+        return ErrorParser()
+    }
+    
+    lazy var commonSession: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpShouldSetCookies = false
+        configuration.headers = .default
+        let manager = Session(configuration: configuration)
+        return manager
+    }()
+    
+    let sessionQueue = DispatchQueue.global(qos: .utility)
+    
+    func makeAuthRequestFatory() -> AuthRequestFactory {
+        let errorParser = makeErrorParser()
+        return Auth(errorParser: errorParser, sessionManager: commonSession, queue: sessionQueue)
+    }
+}
---
+++ GBShop/App/Entity/LoginResult.swift
@@ -0,0 +1,13 @@
+//
+//  LoginResult.swift
+//  GBShop
+//
+//  Created by Максим Лосев on 05.08.2022.
+//
+
+import Foundation
+
+struct LoginResult: Codable {
+    let result: Int
+    let user: User
+}
